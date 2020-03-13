d63549c499beb84fc43be78c514aaae529b0f2cd
andeechu <andychuabsurd@gmail.com>
2020-03-13 13:47:20 -0700

    added HTML elements

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 
 /* Make this function print a random title from the list */
 function updateTitle() {
-    var x = Math.floor(Math.random() * 3);
+    let x = Math.floor(Math.random() * 3);
     const titles = ['title1', 'title2', 'title3']
     const title = document.getElementById('title')
     title.innerHTML = titles[x];
