a6a8a4f45ecb4734cf88c0f7799b0309653e0b55
Allison Chen <bsstmp133@bsscloudla.org>
2020-03-13 12:23:57 -0700

    added code into the updateTitle() function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
-function updateTile()
+function updateTitle()
 {
     // TO DO
     // Make this function change the text to "Title Updated"
+    document.getElementById("title").innerHTML = "Title Updated"
+
 }
