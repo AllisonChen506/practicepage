8f83eeb100dd813b44e1d182ee666ad3cd2f21f6
Allison Chen <bsstmp133@bsscloudla.org>
2020-03-16 14:26:37 -0700

    added onload, a function to return arthimetic

--- scripts.js
+++ scripts.js
@@ -2,32 +2,47 @@
 /* Make this function print a random title from the list */
 function updateTitle() {
     let x = Math.floor(Math.random() * 3);
-    const titles = ['title1', 'title2', 'title3']
+    const titles = ['Hello', 'title2', 'title3']
     const title = document.getElementById('title')
     title.innerHTML = titles[x];
 }
 
-/* Create a function that will calculate the current date */
-function getTime() {
-    // TO DO
+function math()
+{
+    let n1 = document.getElementById('firstnumber').value;
+    let n2 = document.getElementById('secondnumber').value;
+    let operator = document.getElementById('operations').value;
+    let return1 = document.getElementById('returnvalue').value;
+    let return2 = document.getElementById('returnvalue');
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    switch(operator)
+    {
+        case "plus":
+            return1 = n1 + n2;
+            console.log(return1);
+            return2.innerHTML = "The value is " + return1;
+            break;
+        case "subtract":
+            return1 = n1 - n2;
+            console.log(return1);
+            return2.innerHTML = "The value is " + return1;
+            break;
+        case "multiply":
+            return1 = n1 * n2;
+            console.log(return1);
+            return2.innerHTML = "The value is " + return1;
+            break;
+        case "divide":
+            return1 = n1 / n2;
+            console.log(return1);
+            return2.innerHTML = "The value is " + return1;
+            break;
+
+    }
 }
 
-/* Create a function that will add two numbers from input boxes */
-function addNumbers(){
-    // TO DO
+function hello()
+{
+    alert("Welcome");
 }
-
-/* Create a function that will find the difference between two numbers from input boxes */
-function subNumbers(){
-    // TO DO
-}
-
-/* Crate a function that will find the product of two numbers from input boxes */
-function mulNumbers(){
-    // TO DO
-}
-
-/* You can use helper functions if needed. Also, create the function to generate the dropdown menu.
-   The dropdown menu needs to be generated when the page is loaded. If you get confused, do some research
-   on event js functions. onClick() is one of them.
-*/
