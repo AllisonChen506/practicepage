fb040d284b075399cd8e9c52df736c5300e181ae
andeechu <andychuabsurd@gmail.com>
2020-03-16 08:35:32 -0700

    added ToDos to home and scripts

--- scripts.js
+++ scripts.js
@@ -6,3 +6,28 @@ function updateTitle() {
     const title = document.getElementById('title')
     title.innerHTML = titles[x];
 }
+
+/* Create a function that will calculate the current date */
+function getTime() {
+    // TO DO
+}
+
+/* Create a function that will add two numbers from input boxes */
+function addNumbers(){
+    // TO DO
+}
+
+/* Create a function that will find the difference between two numbers from input boxes */
+function subNumbers(){
+    // TO DO
+}
+
+/* Crate a function that will find the product of two numbers from input boxes */
+function mulNumbers(){
+    // TO DO
+}
+
+/* You can use helper functions if needed. Also, create the function to generate the dropdown menu.
+   The dropdown menu needs to be generated when the page is loaded. If you get confused, do some research
+   on event js functions. onClick() is one of them.
+*/
